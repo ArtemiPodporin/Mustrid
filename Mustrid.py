2e06116754f5539155701977eeab7b8fee448fe3
opilane <opilane@HP-CZC2349HTR>
2023-03-28 15:37:11 +0300

    asasd

--- Mustrid.py
+++ Mustrid.py
@@ -1,102 +1,154 @@
-from modulefinder import packagePathMap
-from struct import pack
-from tkinter import * 
-from turtle import width
-from PIL import ImageGrab
-import io
+from tkinter import *
+from math import *
 from random import *
-
-raam = Tk()
-raam.title("Tahvel")
-tahvel = Canvas(raam, width=700, height=500, background="white")
-tahvel.grid()
-
-#1 eest
-tahvel.create_rectangle(500,50,  260,150, fill="blue")
-tahvel.create_rectangle(500,100,   260,150, fill="black")
-tahvel.create_rectangle(500,200,   260,150, fill="white")
-
-#2 bahama
-tahvel.create_rectangle(25,50,  260,150, fill="#24cde0")
-tahvel.create_rectangle(25,100,   260,150, fill="yellow")
-tahvel.create_rectangle(25,200,   260,150, fill="#24cde0")
-tahvel.create_polygon(25,50, 110,125, 25,200, fill="black")
-
-#3 vene 
-tahvel.create_rectangle(25,250,  250,300, fill="white")
-tahvel.create_rectangle(25,350,   250,300, fill="red")
-
-#1
-raam = Tk()
-raam.title("Tahvel")
-tahvel = Canvas(raam, width=600, height=600, background="white")
-k=7
-x0=0
-y0=0
-x1=550
-y1=550
-for i in range(k):
-    x0+=50
-    y0+=50
-    x1-=50
-    y1-=50
-    tahvel.create_rectangle(x0,y0,x1,y1,width=1,outline="blue", fill="red")
-    tahvel.create_oval(x0,y0,x1,y1,width=1, outline="blue", fill="yellow")
-tahvel.grid()
-
-ps = tahvel.postscript(colormode='color')
-x1=tahvel.winfo_width()
-y1=tahvel.winfo_height()
-
-ImageGrab.grab().crop((0,0,x1,y1)).save("image.jpg")
-raam.mainloop()
+from tkinter import font
 
 
-#2
+def Eesti(aken):
+    aken.create_rectangle(0,120,  300,160, fill="blue")
+    aken.create_rectangle(0,160,   300,200, fill="black")
+    aken.create_rectangle(0,200,   300,240, fill="white")
 
-raam = Tk()
-raam.title("Tahvel")
-tahvel = Canvas(raam, width=400, height=400, background="white")
+def Bahamas(aken):
+    aken.create_rectangle(0, 120, 500, 160, fill="#2ca3c7")
+    aken.create_rectangle(0, 160, 500, 200, fill="Yellow")
+    aken.create_rectangle(0, 200, 500, 240, fill="#2ca3c7")
+    aken.create_polygon([0,120],[0,240],[100,180],fill="black")
 
-square_size = 50
-
-for row in range(8):
-    for col in range(8):
-        x1 = col * square_size
-        y1 = row * square_size
-        x2 = x1 + square_size
-        y2 = y1 + square_size
-        if (row+col) % 2 == 0:
-            color = "white"
-        else:
-            color = "black"
-        tahvel.create_rectangle(x1, y1, x2, y2, fill=color)
-
-tahvel.grid()
-
-raam.mainloop()
-
-#3
-colors=["black","cyan","magenta","red","blue","gray","yellow","green","lightblue","pink","gold"]
-raam2=Tk()
-raam2.title("Ringi")
-tahvel2=Canvas(raam2, width=600, height=600, background="white")
-x0=0
-y0=0
-x1=600
-y1=600
-p=5
-for i in range(55):
-    x0+=p 
-    y0+=p
-    x1-=p
-    y1-=p 
-    tahvel2.create_oval(x0,y0,x1,y1, fill=choice(colors))
+def Polski(aken):
+    aken.create_rectangle(0,100,  300,180, fill="white")
+    aken.create_rectangle(0,160,   300,220, fill="red")
+    
 
 
+def Ruuted(aken):
+    k = 10
+    x0 = 0
+    y0 = 0
+    x1 = 300
+    y1 = 300
+    a = 150
+    r = (a ** 2 + a ** 2) ** (1/2)
+    p = (a - r)
+    for i in range(k):
+        x0 += p
+        y0 += p
+        x1 -= p
+        y1 -= p
+        aken.create_rectangle(x0, y0, x1, y1, width=1, outline="blue", fill="red")
+        aken.create_oval(x0, y0, x1, y1, width=1, outline="blue", fill="yellow")  
+        p = r - a
+        r = r - p
+        a = ((r) * sqrt(2)) / 2
 
-tahvel2.grid()
 
-raam2.mainloop()
+def maled(aken):
+    square_size = 35
+    board_size = square_size * 8
+    for i in range(8):
+        for j in range(8):
+            x1 = i * square_size
+            y1 = j * square_size
+            if (i + j) % 2 == 0:
+                color = "white"
+            else:
+                color = "black"
+                square = aken.create_rectangle(x1, y1, x1 + square_size, y1 + square_size, fill=color)
 
-raam.mainloop()
+
+def ring(aken):
+    colors=["black","cyan","magenta","red","blue","gray","yellow","green","lightblue","pink","gold"]
+    x0=0
+    y0=0
+    x1=600
+    y1=600
+    p=5
+    for i in range(60):
+        x0+=p
+        y0+=p
+        x1-=p
+        y1-=p
+        aken.create_oval(x0,y0,x1,y1, fill=choice(colors))
+
+
+def stoplight(aken):
+    aken.create_line(0,0,  0,180,  width=1000, fill="#a3a7ad")
+    fontt = font.Font(family='Helvetica', size=20, weight='bold')
+    aken.create_text(40, 0, font=fontt, anchor=NW)
+    aken.create_line(60,50,  100,50,  width=40 , fill="red")
+    aken.create_line(60,100,  100,100,  width=40 , fill="yellow")
+    aken.create_line(60,150,  100,150,  width=40 , fill="green")
+    aken.create_line(80 ,180,  80,300,  width=8 , fill="black")
+    aken.create_line(30,310,  140,310,  width=4 , fill="black")
+
+
+def valik():
+    num = var.get()
+    if num == 1:
+        raam = Tk()
+        raam.title("lip1")
+        aken = Canvas(raam, width=170, height=380, background="white")
+        Eesti(aken)
+        aken.pack()
+    elif num==2:
+        raam1 = Tk()
+        raam1.title("lip2")
+        aken = Canvas(raam1, width=300, height=380, background="white")
+        Bahamas(aken)
+        aken.pack()
+    elif num==3:
+        raam2 = Tk()
+        raam2.title("lip3")
+        aken = Canvas(raam2, width=300, height=380, background="white")
+        Polski(aken)
+        aken.pack()
+    elif num ==4:
+        raam2 = Tk()
+        raam2.title("ruuti ringud")
+        aken = Canvas(raam2, width=300, height=300, background="white")
+        Ruuted(aken)
+        aken.pack()
+    elif num == 5:
+        raam2 = Tk()
+        raam2.title("maled")
+        aken = Canvas(raam2, width=300, height=300, background="white")
+        maled(aken)
+        aken.pack()
+    elif num==6:
+        raam2 = Tk()
+        raam2.title("ring")
+        aken = Canvas(raam2, width=600, height=600, background="white")
+        ring(aken)
+        aken.pack()
+    elif num==7:
+        raam2 = Tk()
+        raam2.title("valgusfoor")
+        aken = Canvas(raam2, width=150, height=300, background="white")
+        stoplight(aken)
+        aken.pack()
+
+aken = Tk()
+aken.title("Tervist")
+aken.geometry("150x150")
+
+var = IntVar()
+
+r1 = Radiobutton(aken, text="Lipp1", variable=var, value=1, command=valik)
+r2 = Radiobutton(aken, text="Lipp2", variable=var, value=2, command=valik)
+r3 = Radiobutton(aken, text="Lipp3", variable=var, value=3, command=valik)
+r4 = Radiobutton(aken, text="Ruut", variable=var, value=4, command=valik)
+r5 = Radiobutton(aken, text="Maled", variable=var, value=5, command=valik)
+r6 = Radiobutton(aken, text="värviline ring", variable=var, value=6, command=valik)
+r7 = Radiobutton(aken, text="Valgusfoor", variable=var, value=7, command=valik)
+
+lbox = Listbox(aken, height=3, width=33)
+
+r1.grid(row=1, column=0)
+r2.grid(row=2, column=0)
+r3.grid(row=3, column=0)
+r4.grid(row=4, column=0)
+r5.grid(row=5, column=0)
+r6.grid(row=6, column=0)
+r7.grid(row=7, column=0)
+
+aken.mainloop()
